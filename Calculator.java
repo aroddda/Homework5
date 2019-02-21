524fdbbef381adf2ccd9dd9fe06ad2df081cd66b
amare <deona@gmail.com>
2019-02-21 17:08:29 -0600

    Homework5

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+package edu.dmacc.codedsm.hw5;
+
+public class Calculator {
+
+
+    public static void main(String[] args) {
+
+
+        double z  = Math.sqrt(27);
+        System.out.println("sqrt27 = " + z);
+        int x = Math.round(28/5);
+        System.out.println(x);
+        double y = Math.pow(2.2,4);
+        System.out.println("pow4 = " + y);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
